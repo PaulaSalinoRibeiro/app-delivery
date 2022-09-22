e8a3b31f2f664a9dafe71ad930f35ee8a8073654
IgorHamzi <igorwhamzi@gmail.com>
2022-09-22 17:38:41 -0300

    feat: create Navbar

--- back-end/src/middlewares/errorMiddleware.js
+++ back-end/src/middlewares/errorMiddleware.js
@@ -8,6 +8,7 @@ const httpStatusCode = {
 
 const erroMiddleware = (err, _req, res, _next) => {
   const { code } = err;
+  console.log(err);
   if (!code) return res.status(httpStatusCode.InternalServer).json({ message: err.message });
   res.status(httpStatusCode[code]).json({ message: err.message });
 };
