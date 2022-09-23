e749a19d1505440b84912b843bbc248e43cfa3d8
Paula <paulasalinor@gmail.com>
2022-09-22 21:20:25 -0300

    feat: add styled NavBar component

---
+++ front-end/src/pages/OrderPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import NavBar from '../../components/NavBar';
+
+import * as S from './styled';
+
+export default function OrderPage() {
+  return (
+    <S.Container>
+      <NavBar />
+    </S.Container>
+  );
+}
---
+++ front-end/src/pages/OrderPage/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+
+`;
+
+export const Title = styled.h1``;
