353d195e7da2e4176b2968acbb2e44c6088a70b3
Lucas Lopes <lucascaldaslopes15@gmail.com>
2022-09-25 23:36:42 -0300

    fix req 14

--- back-end/src/controllers/imageController.js
+++ back-end/src/controllers/imageController.js
@@ -1,6 +1,6 @@
 const getImage = async (req, res) => {
   res.status(200).sendFile(
-    '/home/devlopes/trybe-projects/sd-019-c-project-delivery-app/back-end/public/no_image.jpg',
+    '/home/devlopes/trybe-projects/sd-019-c-project-delivery-app/back-end/src/images/no_image.jpg',
   );
 };
 
